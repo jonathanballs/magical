fb22ac1f1b0c3df6cc1839c6362d510519d78295
Jonathan Balls <jonathanballs@protonmail.com>
2024-05-30 15:36:06 +0100

    Use Serializer.Kv to serialize calendar properties

--- lib/magical/serializer/calendar_serializer.ex
+++ lib/magical/serializer/calendar_serializer.ex
@@ -2,6 +2,7 @@ defmodule Magical.Serializer.CalendarSerializer do
   alias Magical.Calendar
 
   alias Magical.Serializer.EventSerializer
+  alias Magical.Serializer.Kv
 
   @defaults %{
     version: "2.0",
@@ -26,31 +27,31 @@ defmodule Magical.Serializer.CalendarSerializer do
   end
 
   defp do_serialize(%{version: version} = calendar) do
-    "VERSION:#{version}\n" <> do_serialize(Map.delete(calendar, :version))
+    Kv.serialize(:version, version) <> do_serialize(Map.delete(calendar, :version))
   end
 
   defp do_serialize(%{method: method} = calendar) do
-    "METHOD:#{method}\n" <> do_serialize(Map.delete(calendar, :method))
+    Kv.serialize(:method, method) <> do_serialize(Map.delete(calendar, :method))
   end
 
   defp do_serialize(%{prodid: prodid} = calendar) do
-    "PRODID:#{prodid}\n" <> do_serialize(Map.delete(calendar, :prodid))
+    Kv.serialize(:prodid, prodid) <> do_serialize(Map.delete(calendar, :prodid))
   end
 
   defp do_serialize(%{calscale: calscale} = calendar) do
-    "CALSCALE:#{calscale}\n" <> do_serialize(Map.delete(calendar, :calscale))
+    Kv.serialize(:calscale, calscale) <> do_serialize(Map.delete(calendar, :calscale))
   end
 
   defp do_serialize(%{name: name} = calendar) do
-    "X-WR-CALNAME:#{name}\n" <> do_serialize(Map.delete(calendar, :name))
+    Kv.serialize(:"X-WR-CALNAME", name) <> do_serialize(Map.delete(calendar, :name))
   end
 
   defp do_serialize(%{description: description} = calendar) do
-    "X-WR-CALDESC:#{description}\n" <> do_serialize(Map.delete(calendar, :description))
+    Kv.serialize(:"X-WR-CALDESC", description) <> do_serialize(Map.delete(calendar, :description))
   end
 
   defp do_serialize(%{time_zone: time_zone} = calendar) do
-    "X-WR-TIMEZONE:#{time_zone}\n" <> do_serialize(Map.delete(calendar, :time_zone))
+    Kv.serialize(:"X-WR-TIMEZONE", time_zone) <> do_serialize(Map.delete(calendar, :time_zone))
   end
 
   defp do_serialize(%{events: events}) do
--- lib/magical/serializer/kv.ex
+++ lib/magical/serializer/kv.ex
@@ -1,2 +1,17 @@
 defmodule Magical.Serializer.Kv do
+  def serialize(name, value, params \\ %{}) do
+    name =
+      name
+      |> Atom.to_string()
+      |> String.replace("_", "-")
+      |> String.upcase()
+
+    "#{name}#{serialize_params(params)}:#{value}\n"
+  end
+
+  defp serialize_params(params) do
+    params
+    |> Enum.map(fn {k, v} -> ";#{String.upcase(k)}=#{v}" end)
+    |> Enum.join("")
+  end
 end
